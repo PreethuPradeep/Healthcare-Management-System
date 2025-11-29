54722be2389aefc545956b3b8252bfc893585a35
PreethuPradeep <207873069+PreethuPradeep@users.noreply.github.com>
2025-11-29 13:09:27 +0530

    merged dev into my feature branch

---
+++ backend/HealthCare/HealthCare/Models/Doctor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HealthCare.Models
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User? User { get; set; }
+
+        [ForeignKey("Specialization")]
+        public int SpecializationId { get; set; }
+        public Specialization? Specialization { get; set; }
+
+        public decimal Fee { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
